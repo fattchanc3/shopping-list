fbd624ebc6986aef1bfe7b376f567d66132ba9e5
fattchanc3 <45506833+fattchanc3@users.noreply.github.com>
2019-04-11 02:00:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// 1. If you click on the list item, it toggles the .done  class on and off.
-// 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
-// 3. When adding a new list item, it automatically adds the delete button next to it. 
-
-
-
-
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 // var del = document.getElementById("del");
